b2db29a84d36db58320b08998c2eca7c33a582d9
“alexandro591” <alexandrotapiaflores@gmail.com>
2019-12-31 02:42:29 -0500

    second version

--- src/index.js
+++ src/index.js
@@ -5,12 +5,9 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const axios = require("axios");
 
-var fs = require('fs');
-
 calificaME.use(bodyParser.json());
 calificaME.use(bodyParser.urlencoded({ extended: true }));
 
-/*function to get body from url
 async function getData(url){
     try {
         var body = await axios.get(url);
@@ -20,33 +17,17 @@ async function getData(url){
     }
     return body;
 };
-*/
-
-router.get("/resetdepublicaciones",(request,response)=>{
-});
-
-router.get("/publicaciones",(request,response)=>{
-    fs.readFile('public/publicaciones.html', function(err, data) {
-        response.write(data);
-        response.end();
-    });
-});
 
 router.get("/",(request,response)=>{
-    let top="";
-    let publicaciones="";
-    let bottom="";
-    fs.readFile('public/top.html', function(err, data) {
-        top=data.toString();
-        fs.readFile('public/publicaciones.html', function(err, data) {
-            publicaciones=data.toString();
-            fs.readFile('public/bottom.html', function(err, data) {
-                bottom=data.toString();
-                response.write(top+publicaciones+bottom);
-                response.end();
-            });
+    getData('https://raw.githubusercontent.com/alexandro591/CalificaME/master/public/top.html')
+    .then(res =>{
+        response.write(res);
+        getData('https://raw.githubusercontent.com/alexandro591/CalificaME/master/public/bottom.html')
+        .then(res =>{
+            response.write(res);
+            response.end();
         });
-    })
+    });
 
 });
 
@@ -59,7 +40,8 @@ router.post("/",function(request,response){
     let calificacion = request.body.calificacion;
     var datetime = new Date();
     comentario = comentario.replace(/\n/g,"<br>");
-    publicacion ='<div class="row">\
+
+    /*publicacion ='<div class="row">\
                         <div class="col-sm text-center">\
                             <h4>'+nombre+'<br></h4>\
                             '+universidad+'<br>\
@@ -72,8 +54,8 @@ router.post("/",function(request,response){
                             <p>Date:'+datetime.toISOString().slice(0,10)+'</p>\
                         </div>\
                     </div><hr>';
-    var fd = fs.openSync('public/publicaciones.html', 'a+');
-    fs.write(fd, publicacion, 0, publicacion.length, 0);
+    */
+
     response.write("ok");
     response.end();
 });
